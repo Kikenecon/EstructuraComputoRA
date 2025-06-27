62a2dca42c09edc148c9275935c5037e9308fda5
IJuanDHerrera <juangalviz9648@gmail.com>
2025-06-27 17:35:00 -0500

    Revert "agregando componentes a las preguntas"
    This reverts commit 9ccace3863c328245be5ec8150f4aed056e0dded.

--- AppCartilla_2.0/Assets/Scrips/Preguntas/GameModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Trivia/Create Game Model")]
-public class GameModel : ScriptableObject
-{
-    public List<CategoryModel> categories;
-}
